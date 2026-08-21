8588b0d0aaa50b15cc977b594577561dae79e223
lorenzo.gao <lorenzo.gao@ug.uchile.cl>
2026-08-20 23:28:06 -0400

    feat: agregar funciones __any__ de etapa3.

--- labs/Lab3/src/pixellab/imagen.py
+++ labs/Lab3/src/pixellab/imagen.py
@@ -20,44 +20,74 @@ class Imagen:
             )
         elif img.ndim != 3:
             raise ValueError(
-                "Debes entregar un arreglo de numpy de 3 dimensiones"
+                "Debes entregar un arreglo de numpy con 3 dimensiones"
             )
         elif img.shape[-1] != 3:
-            raise ValueError("Debes entregar un arreglo de numpy de 3 canales")
+            raise ValueError("Debes entregar un arreglo de numpy con 3 canales")
         self.imagen = img
 
     def __add__(self, other: int | float | np.ndarray | Imagen) -> Imagen:
         # Su código aquí
-        raise NotImplementedError(
-            "Completen __add__ antes de ejecutar el programa."
-        )
+        img = self.imagen.copy()
+        try:
+            other_value = obtener_operador(img, other)
+            return Imagen(saturar_check(img + other_value))
+        except ValueError as err:
+            raise ValueError("Las dimensiones no calzan") from err
 
     def __radd__(self, other: int | float | np.ndarray | Imagen) -> Imagen:
         # Su código aquí
-        raise NotImplementedError(
-            "Completen __radd__ antes de ejecutar el programa."
-        )
+        return self.__add__(other)
 
     def __sub__(self, other: int | float | np.ndarray | Imagen) -> Imagen:
         # Su código aquí
-        raise NotImplementedError(
-            "Completen __sub__ antes de ejecutar el programa."
-        )
+        img = self.imagen.copy()
+        try:
+            other_value = obtener_operador(img, other)
+            return Imagen(saturar_check(img - other_value))
+        except ValueError as err:
+            raise ValueError("Las dimensiones no calzan") from err
 
     def __rsub__(self, other: int | float | np.ndarray | Imagen) -> Imagen:
         # Su código aquí
-        raise NotImplementedError(
-            "Completen __rsub__ antes de ejecutar el programa."
-        )
+        img = self.imagen.copy()
+        try:
+            other_value = obtener_operador(img, other)
+            return Imagen(saturar_check(other_value - img))
+        except ValueError as err:
+            raise ValueError("Las dimensiones no calzan") from err
 
     def __mul__(self, other: int | float | np.ndarray | Imagen) -> Imagen:
         # Su código aquí
-        raise NotImplementedError(
-            "Completen __mul__ antes de ejecutar el programa."
-        )
+        img = self.imagen.copy()
+        try:
+            other_value = obtener_operador(img, other)
+            if isinstance(other_value, np.ndarray):
+                other_dim = other.ndim
+                if img.shape[other_dim:] != other.shape[:other_dim]:
+                    raise ValueError("no se coinciden las dimensiones.")
+            return Imagen(saturar_check(img * other))
+        except ValueError as err:
+            raise ValueError("Las dimensiones no calzan") from err
 
     def __rmul__(self, other: int | float | np.ndarray | Imagen) -> Imagen:
         # Su código aquí
-        raise NotImplementedError(
-            "Completen __rmul__ antes de ejecutar el programa."
-        )
+        return self.__mul__(other)
+
+
+def obtener_operador(
+    img: np.ndarray, other: int | float | np.ndarray | Imagen
+) -> int | float | np.ndarray:
+    img_size = img.shape
+    if isinstance(other, Imagen):
+        return obtener_operador(img, other.imagen)
+    elif isinstance(other, np.ndarray) and (img_size != other.shape):
+        raise ValueError("Debes entregar un arreglo de misma dimension")
+    return other
+
+
+def saturar_check(img: np.ndarray):
+    imagen = img.copy().astype(int)
+    imagen[imagen > 255] = 255
+    imagen[imagen < 0] = 0
+    return imagen
